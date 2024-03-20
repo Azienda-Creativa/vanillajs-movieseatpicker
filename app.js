c91661c5c27ec6960c4fdce558fab2fa52181477
Azienda-Creativa <116496155+Azienda-Creativa@users.noreply.github.com>
2024-03-20 18:55:29 +0100

    local storage, to finish implementation and test

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const container = document.querySelector(".container")
+const seats = document.querySelectorAll(".seat")
 const screen = document.querySelectorAll(".row .seat:not(.occupied)")
 const count = document.getElementById("count")
 const total = document.getElementById("total")
@@ -6,16 +7,25 @@ const movieSelect = document.getElementById("movie")
 // + transforms string into number
 let ticketPrice = +movieSelect.value
 
+const setMovieData = (movieIndex, moviePrice) => {
+  localStorage.setItem("selectedMovieIndex", movieIndex)
+  localStorage.setItem("selectecMoviePrice", moviePrice)
+}
+
 const updateSelectedCount = () => {
   const selectedSeats = document.querySelectorAll(".row .seat.selected")
   const selectedSeatsCount = selectedSeats.length
   count.innerText = selectedSeatsCount
   total.innerText = selectedSeatsCount * ticketPrice
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat))
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex))
 }
 
 //select movie
 movieSelect.addEventListener("change", (e) => {
   ticketPrice = +e.target.value
+  setMovieData(e.target.selectedIndex, e.target.value)
+  console.log(e.target.selectedIndex)
   updateSelectedCount()
 })
 // select seat
