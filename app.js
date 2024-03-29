58204d69a9cf1a05a31937c3d72f1d889217ae51
Azienda-Creativa <116496155+Azienda-Creativa@users.noreply.github.com>
2024-03-29 12:05:23 +0100

    fixing bugs + readme

--- app.js
+++ app.js
@@ -7,25 +7,44 @@ const movieSelect = document.getElementById("movie")
 // + transforms string into number
 let ticketPrice = +movieSelect.value
 
-let setMovieData = (movieIndex, moviePrice) => {
+const setMovieData = (movieIndex, moviePrice) => {
   localStorage.setItem("selectedMovieIndex", movieIndex)
   localStorage.setItem("selectecMoviePrice", moviePrice)
 }
 
-let updateSelectedCount = () => {
+const updateSelectedCount = () => {
   const selectedSeats = document.querySelectorAll(".row .seat.selected")
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat))
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex))
+  //console.log(seatsIndex)
   const selectedSeatsCount = selectedSeats.length
   count.innerText = selectedSeatsCount
   total.innerText = selectedSeatsCount * ticketPrice
-  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat))
-  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex))
 }
+// get data fomr local storage
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"))
+
+  if (selectedSeats && selectedSeats.length > 0) {
+    // Populate UI with selected seats
+    console.log(selectedSeats)
+  } else {
+    // Handle case where no selected seats are found in localStorage
+    console.log("No selected seats found in localStorage")
+  }
+}
+// if (selectedSeats !== null && selectedSeats.length > 0) {
+//   seats.forEach((seat, index) => {
+//     if (selectedSeats.indexOf(index) > -1) {
+//       seat.classList.add("selected")
+//     }
+//   })
+// }
 
 //select movie
 movieSelect.addEventListener("change", (e) => {
   ticketPrice = +e.target.value
   setMovieData(e.target.selectedIndex, e.target.value)
-  console.log(e.target.selectedIndex, e.target.value)
   updateSelectedCount()
 })
 // select seat
